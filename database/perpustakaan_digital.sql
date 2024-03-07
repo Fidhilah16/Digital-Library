ccc0ed07297b6d8b772a2c259341068a277ed116
Rafi Akmal Fadhilah <161666525+Fidhilah16@users.noreply.github.com>
2024-03-07 19:19:53 +0700

    Add files via upload

---
+++ database/perpustakaan_digital.sql
@@ -0,0 +1,205 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 07, 2024 at 12:59 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `perpustakaan_digital`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_buku`
+--
+
+CREATE TABLE `tb_buku` (
+  `bukuID` int(11) NOT NULL,
+  `kategoriID` int(11) DEFAULT NULL,
+  `judul` varchar(255) DEFAULT NULL,
+  `kategori` varchar(255) DEFAULT NULL,
+  `file` varchar(255) DEFAULT NULL,
+  `penulis` varchar(255) DEFAULT NULL,
+  `penerbit` varchar(255) DEFAULT NULL,
+  `tahun_terbit` varchar(255) DEFAULT NULL,
+  `sinopsis` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_buku`
+--
+
+INSERT INTO `tb_buku` (`bukuID`, `kategoriID`, `judul`, `kategori`, `file`, `penulis`, `penerbit`, `tahun_terbit`, `sinopsis`) VALUES
+(2, NULL, 'Boruto - TBV', 'Komik', 'Boruto TBV.jpg', 'Mikkio Ikemoto', 'Shueisha', '2017', 'Boruto Two Blue Vortex');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_kategori`
+--
+
+CREATE TABLE `tb_kategori` (
+  `kategoriID` int(11) NOT NULL,
+  `nama_kategori` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_kategori`
+--
+
+INSERT INTO `tb_kategori` (`kategoriID`, `nama_kategori`) VALUES
+(4, 'Komik'),
+(5, 'Novel'),
+(6, 'Novel');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_peminjaman`
+--
+
+CREATE TABLE `tb_peminjaman` (
+  `peminjamanID` int(11) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `judul` varchar(255) DEFAULT NULL,
+  `tgl_peminjaman` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `tgl_pengembalian` varchar(255) DEFAULT NULL,
+  `status` enum('Dipinjam','Tersedia') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_ulasan`
+--
+
+CREATE TABLE `tb_ulasan` (
+  `ulasanID` int(11) NOT NULL,
+  `username` int(2) DEFAULT NULL,
+  `judul` int(255) DEFAULT NULL,
+  `ulasan` text DEFAULT NULL,
+  `rating` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `userID` int(11) NOT NULL,
+  `nama_lengkap` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `domisili` varchar(255) DEFAULT NULL,
+  `role` enum('Administrator','Petugas','Peminjam') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+INSERT INTO `tb_user` (`userID`, `nama_lengkap`, `username`, `password`, `email`, `domisili`, `role`) VALUES
+(1, 'Rafi Akmal', 'akmal', '272874d450b7f8381b1174133ac62b40', 'akmal@gmail.com', 'Bekasi Utara', 'Administrator'),
+(2, 'peminjam', 'peminjam', '55f3894bc5fc71fead8412d321c2952c', 'peminjam@gmail.com', 'Tambun Selatan', 'Peminjam'),
+(3, 'petugas', 'petugas', 'afb91ef692fd08c445e8cb1bab2ccf9c', 'petugas@gmail.com', 'Jakarta Selatan', 'Petugas');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_buku`
+--
+ALTER TABLE `tb_buku`
+  ADD PRIMARY KEY (`bukuID`),
+  ADD KEY `kategoriID` (`kategoriID`);
+
+--
+-- Indexes for table `tb_kategori`
+--
+ALTER TABLE `tb_kategori`
+  ADD PRIMARY KEY (`kategoriID`);
+
+--
+-- Indexes for table `tb_peminjaman`
+--
+ALTER TABLE `tb_peminjaman`
+  ADD PRIMARY KEY (`peminjamanID`);
+
+--
+-- Indexes for table `tb_ulasan`
+--
+ALTER TABLE `tb_ulasan`
+  ADD PRIMARY KEY (`ulasanID`);
+
+--
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`userID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_buku`
+--
+ALTER TABLE `tb_buku`
+  MODIFY `bukuID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tb_kategori`
+--
+ALTER TABLE `tb_kategori`
+  MODIFY `kategoriID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tb_peminjaman`
+--
+ALTER TABLE `tb_peminjaman`
+  MODIFY `peminjamanID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_ulasan`
+--
+ALTER TABLE `tb_ulasan`
+  MODIFY `ulasanID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_buku`
+--
+ALTER TABLE `tb_buku`
+  ADD CONSTRAINT `tb_buku_ibfk_1` FOREIGN KEY (`kategoriID`) REFERENCES `tb_kategori` (`kategoriID`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
